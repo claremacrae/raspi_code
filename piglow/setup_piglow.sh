139012fe437ee2a93022f3c202e1c62e79ececf5
claremacrae <github@cfmacrae.fastmail.co.uk>
2013-12-29 18:43:26 +0000

    Add commands to download piglow class

--- piglow/setup_piglow.sh
+++ piglow/setup_piglow.sh
@@ -10,7 +10,11 @@ grep -q '^i2c-bcm2708' /etc/modules || echo 'ERROR Add i2c-bcm2708 to /etc/modul
 grep -q '^i2c-dev'     /etc/modules || echo 'ERROR Add i2c-dev     to /etc/modules'
 
 # To allow use of I2C with python:
-sudo apt-get install python-smbus
+sudo apt-get install python-smbus python-psutil -y
+
+# Taken from https://github.com/Boeeerb/PiGlow
+wget https://raw.github.com/Boeeerb/PiGlow/master/piglow.py
+wget https://raw.github.com/Boeeerb/PiGlow/master/Examples/test.py
 
 echo Now reboot the pi
 
