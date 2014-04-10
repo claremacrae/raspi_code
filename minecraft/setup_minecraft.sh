f142baf9b0ef13a8388ea5d4f7e261c7537076fa
claremacrae <github@cfmacrae.fastmail.co.uk>
2014-04-10 19:03:38 +0000

    Fixed typo in minecraft script

--- minecraft/setup_minecraft.sh
+++ minecraft/setup_minecraft.sh
@@ -2,5 +2,5 @@
 
 # From https://docs.google.com/document/d/17zjTIY6iO8amK21jKYJVOQruycdDRxusavw-RBBW-Is/mobilebasic?pli=1
 
-wget https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft- pi-0.1.1.tar.gz
+wget https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft-pi-0.1.1.tar.gz
 tar -zxvf minecraft-pi-0.1.1.tar.gz
