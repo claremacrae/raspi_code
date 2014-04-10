4cf55eb9d137453c8ddc22921c9d0388bc0e971f
claremacrae <github@cfmacrae.fastmail.co.uk>
2014-04-10 21:31:25 +0000

    Install Minecraft in ~, instead of git dir

--- minecraft/setup_minecraft.sh
+++ minecraft/setup_minecraft.sh
@@ -2,5 +2,6 @@
 
 # From https://docs.google.com/document/d/17zjTIY6iO8amK21jKYJVOQruycdDRxusavw-RBBW-Is/mobilebasic?pli=1
 
+cd ~
 wget https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft-pi-0.1.1.tar.gz
 tar -zxvf minecraft-pi-0.1.1.tar.gz
