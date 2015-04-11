6046b712f7c6453724e4ba245aca78574eca1620
claremacrae <github@cfmacrae.fastmail.co.uk>
2015-04-11 19:53:28 +0000

    Another stab at a python setup script for matplotlib

---
+++ setup/python-setup-v2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt-get install python-setuptools -y
+sudo apt-get install python-virtualenv -y
+
+#easy_install --version
+#distribute 0.6.24dev-r0
+
+#pip --version
+#pip 1.1 from /usr/lib/python2.7/dist-packages (python 2.7)
+
+#virtualenv --version
+#1.7.1.2
+
+sudo apt-get install python-matplotlib -y
