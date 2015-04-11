9732429f074430a05f2bac7e10ccbef1c5e3d971
claremacrae <github@cfmacrae.fastmail.co.uk>
2015-04-11 13:35:06 +0000

    Add setup of required python modules

--- setup/python-setup.sh
+++ setup/python-setup.sh
@@ -3,3 +3,12 @@ apt-get install python-pip -y
 apt-get install python3-pip -y
 apt-get install python-dev
 
+pip install pillow
+easy_install -U distribute
+pip install matplotlib
+pip install nose
+
+# to test:     python
+# >>> import numpy
+# >>> import matplotlib
+# >>> import nose
