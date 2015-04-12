32b242cab9d3e9a83c69c90b155920a77a318ed7
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2015-04-12 13:15:41 +0000

    Add another, simpler, matplotlib example

---
+++ programming/python/matplotlib/sin_and_cos_1.py
@@ -0,0 +1,11 @@
+# from http://www.labri.fr/perso/nrougier/teaching/matplotlib/
+
+from pylab import*
+
+X = np.linspace(-np.pi, np.pi, 256,endpoint=True)
+C,S = np.cos(X), np.sin(X)
+
+plot(X,C)
+plot(X,S)
+
+show()
