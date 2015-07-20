3789c8d4e707e01f90b7f49f42c87de379f1bd20
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2015-07-20 22:13:00 +0100

    Start of script to set up Sonic Pi development environment

---
+++ setup/setup_sonicpi_dev.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# from https://github.com/samaaron/sonic-pi/blob/master/INSTALL.md#raspberry-pi
+
+for dep in supercollider ruby1.9.3 libqscintilla2-8 libqscintilla2-dev qt4-dev-tools cmake ruby-dev libffi-dev ; do
+    echo $dep
+    sudo apt-get -y install $dep
+done
+
