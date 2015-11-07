0824bb4025d00d9e435c162a0b1931d448baf7c9
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2015-11-07 21:54:30 +0000

    Add moving of marble on x axis - currently brokwn

--- hardware/sense_hat/marble_maze.py
+++ hardware/sense_hat/marble_maze.py
@@ -24,10 +24,21 @@ maze = [[r,r,r,r,r,r,r,r],
         [r,b,b,r,b,b,b,r],
         [r,r,r,r,r,r,r,r]]
 
-sense.set_pixels(sum(maze,[]))
+def move_marble(pitch, roll, x, y):
+    new_x = x
+    new_y = y
+    if 1 < pitch < 179:
+        new_x -= 1
+    elif 359 > pitch > 181:
+        new_x += 1
+    return new_x, new_y
 
 game_over = False
 
 while not game_over:
+    pitch = sense.get_orientation()['pitch']
+    roll = sense.get_orientation()['roll']
+    x,y = move_marble(pitch, roll, x, y)
     maze[y][x] = w
     sense.set_pixels(sum(maze,[]))
+
