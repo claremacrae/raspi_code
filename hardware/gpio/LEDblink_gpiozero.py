5f2281d494d762923002d7ddfd8f62192a27c2d2
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2015-11-08 21:29:12 +0000

    Blink an LED using gpiozero

---
+++ hardware/gpio/LEDblink_gpiozero.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+import time
+
+# GPIO 24 ... LED ... 470 ohm resistor ... GND
+
+led = LED(24)
+try:
+    led.blink()
+    time.sleep(20)
+except KeyboardInterrupt:
+    led.off()
+
