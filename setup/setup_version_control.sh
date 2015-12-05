c1284fcd431fe9cc1cdd10a0a98427d3efb9111e
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2015-12-05 20:54:55 +0000

    Better comment describing gitg

--- setup/setup_version_control.sh
+++ setup/setup_version_control.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# gitg provides a UI to view history and commit changes.
+# gitg provides a UI to view and commit changes, and gets added to Programming menu.
 # gitk provides a UI to view history.
 
 sudo apt-get install gitg gitk -y
