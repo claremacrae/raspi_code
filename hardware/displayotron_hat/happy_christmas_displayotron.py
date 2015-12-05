ddecae674a362263afa01c6eb4dd2aa1c28bc26c
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2015-12-05 20:45:30 +0000

    First stab at a displayotron Christmas message

---
+++ hardware/displayotron_hat/happy_christmas_displayotron.py
@@ -0,0 +1,12 @@
+import dothat.lcd as lcd
+import dothat.backlight as backlight
+from time import sleep
+
+lcd.set_cursor_position(0, 1)
+lcd.write('Happy Christmas!')
+
+#for x in range(50):
+#    backlight.hue(x / 50.0)
+#    sleep(0.05)
+
+backlight.rgb(0, 255, 0)
