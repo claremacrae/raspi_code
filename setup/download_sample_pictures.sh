fb4f970b7b1053870603360c4ea9466965129ed1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2015-12-06 20:22:24 +0000

    Trial script to download some CC0 pictures
    This is intended to make a slide show on a PiTFT

---
+++ setup/download_sample_pictures.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# https://www.flickr.com/search/?q=christmas&l=9
+OUTPUT_DIR=~/Pictures/Christmas
+#640 x 
+mkdir -p $OUTPUT_DIR
+cd       $OUTPUT_DIR
+wget "https://farm1.staticflickr.com/670/23361066915_6be6fcd758_z_d.jpg"
+wget "https://farm8.staticflickr.com/7479/15475695714_d417383381_z_d.jpg"
+wget "https://c2.staticflickr.com/8/7469/15842503347_85dc201dfb_z_d.jpg"
