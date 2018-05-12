1ad0c5d8b79490147c2b2383491232441dbd54fc
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2018-05-12 17:55:47 +0100

    Add script to set up Explorer PHAT

---
+++ setup/setup_explorer_phat.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# from https://learn.pimoroni.com/tutorial/tanya/building-the-tiny-4wd
+
+curl https://get.pimoroni.com/explorerhat | bash
+sudo pip install inputs
+#wget https://raw.githubusercontent.com/Coretec-Robotics/Tiny_4wd/master/TinyPirate.py
