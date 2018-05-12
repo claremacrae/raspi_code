7c9ccaaaaf609a34fce169d360a2a93cf0401926
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-05-12 17:32:18 +0100

    Add install of git-gui, and add comments explaining the git commands

--- setup/setup_version_control.sh
+++ setup/setup_version_control.sh
@@ -3,6 +3,9 @@
 # gitg provides a UI to view and commit changes, and gets added to Programming menu.
 # gitk provides a UI to view history.
 
-sudo apt-get install gitg gitk -y
+# gitg - repository viewer
+# gitk - for browsing history
+# git gui - for committing a file
+sudo apt-get install gitg gitk git-gui -y
 
 sudo apt-get install -y mercurial tortoisehg kdiff3 meld
