7ba9a8e7166a57c9fc8e487f3fd0859c495251e5
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-05-12 18:02:59 +0000

    Add installation of approxeng.input library - which is a better library for using game controllers

--- setup/setup_explorer_phat.sh
+++ setup/setup_explorer_phat.sh
@@ -3,5 +3,11 @@
 # from https://learn.pimoroni.com/tutorial/tanya/building-the-tiny-4wd
 
 curl https://get.pimoroni.com/explorerhat | bash
+
+# https://pypi.org/project/inputs/
 sudo pip install inputs
+
+# From https://github.com/ApproxEng/approxeng.input
+sudo pip install approxeng.input
+
 #wget https://raw.githubusercontent.com/Coretec-Robotics/Tiny_4wd/master/TinyPirate.py
