74de86bf0cb720955e8fde5a70b3ac56f14ba82c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2018-05-13 09:58:24 +0100

    Remove need to escape special chars in heredoc

--- setup/setup_dotfiles.sh
+++ setup/setup_dotfiles.sh
@@ -7,7 +7,7 @@ set -u
 
 BASH_ALIASES=~/.bash_aliases
 if [ ! -f $BASH_ALIASES ]; then
-    cat > $BASH_ALIASES <<EOF
+    cat > $BASH_ALIASES << 'EOF'
 
 alias ll='ls -l'
 alias lla='ls -la'
@@ -15,20 +15,20 @@ alias la='ls -A'
 alias l='ls -CF'
 alias rt='ls -lsrta'
 
-alias gstat='for dir in * ; do cd \$dir; git status; echo \$dir; echo " " ; cd ..; done'
-alias gdiff='for dir in * ; do cd \$dir; git diff  ; echo \$dir; echo " " ; cd ..; done'
+alias gstat='for dir in * ; do cd $dir; git status; echo $dir; echo " " ; cd ..; done'
+alias gdiff='for dir in * ; do cd $dir; git diff  ; echo $dir; echo " " ; cd ..; done'
 
 CDPATH="."
-for dir in \\
-    /home/pi/ \\
-    /home/pi/develop \\
-    /home/pi/develop/raspi_code/ \\
-    /home/pi/develop/raspi_code/hardware/ \\
+for dir in \
+    /home/pi/ \
+    /home/pi/develop \
+    /home/pi/develop/raspi_code/ \
+    /home/pi/develop/raspi_code/hardware/ \
     /home/pi/develop/raspi_code/programming/ ; do
-    if [ ! -d "\$dir" ]; then
+    if [ ! -d "$dir" ]; then
         continue
     fi
-    CDPATH="\$CDPATH:\$dir"
+    CDPATH="$CDPATH:$dir"
 done
 EOF
 
