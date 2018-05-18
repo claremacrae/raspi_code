a1162617c5c15e2021db64d88fb9e68efe1900b7
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-05-18 13:24:40 +0000

    Add some TODOs to improve connection logging

--- tools/monitor_connection_speed/get_one_result.sh
+++ tools/monitor_connection_speed/get_one_result.sh
@@ -17,5 +17,17 @@ if [[ ! -f $output_file ]] ; then
     $script_dir/speedtest-cli-extras/bin/speedtest-csv --header > $output_file
 fi
 
+# TODO Read https://www.raspberrypi.org/learning/networking-lessons/lessons/
+# TODO Include a column for hostname - so I can use hostname to indicate location
+# TODO Include a column for connection type - WiFi or Ethernet
+# TODO Include a column for Raspberry Pi type
+#  For WiFi: iwgetid --raw      ... gives ESSID:
+#            iwconfig           ... gives more info
+#  For Ethernet: ifconfig eth0
+#                ifconfig -s
+#SEE ALSO
+#       route(8), netstat(8), arp(8), rarp(8), iptables(8), ifup(8), interfaces(5).
+#       http://physics.nist.gov/cuu/Units/binary.html - Prefixes for binary multiples
+
 $script_dir/speedtest-cli-extras/bin/speedtest-csv --no-share >> $output_file
 
