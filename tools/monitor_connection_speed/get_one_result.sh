ae042b7b9f98c2e02a259e487e3fea878e1d6c54
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-05-18 14:00:47 +0000

    Include hostname in logs of connection speed

--- tools/monitor_connection_speed/get_one_result.sh
+++ tools/monitor_connection_speed/get_one_result.sh
@@ -14,11 +14,10 @@ echo Sending output to $output_file
 # Write headers to output file, if it does not yet exist
 if [[ ! -f $output_file ]] ; then
     echo Creating $output_file
-    (echo -ne "pi_revision\\t"; $script_dir/speedtest-cli-extras/bin/speedtest-csv --header ) > $output_file
+    (echo -ne "hostname\\tpi_revision\\t"; $script_dir/speedtest-cli-extras/bin/speedtest-csv --header ) > $output_file
 fi
 
 # TODO Read https://www.raspberrypi.org/learning/networking-lessons/lessons/
-# TODO Include a column for hostname - so I can use hostname to indicate location
 # TODO Include a column for connection type - WiFi or Ethernet
 #  For WiFi: iwgetid --raw      ... gives ESSID:
 #            iwconfig           ... gives more info
@@ -28,7 +27,7 @@ fi
 #       route(8), netstat(8), arp(8), rarp(8), iptables(8), ifup(8), interfaces(5).
 #       http://physics.nist.gov/cuu/Units/binary.html - Prefixes for binary multiples
 
+hostname=`hostname`
 # For meaning of pi_revision values, see https://elinux.org/RPi_HardwareHistory
 pi_revision=`cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'`
-(echo -ne "$pi_revision\\t"; $script_dir/speedtest-cli-extras/bin/speedtest-csv --no-share ) >> $output_file
-
+(echo -ne "$hostname\\t$pi_revision\\t"; $script_dir/speedtest-cli-extras/bin/speedtest-csv --no-share ) >> $output_file
