083d41619ea82e08c99ffcb74840641c08b8c2f3
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-05-18 13:50:38 +0000

    Include a column for Raspberry Pi revision

--- tools/monitor_connection_speed/get_one_result.sh
+++ tools/monitor_connection_speed/get_one_result.sh
@@ -14,13 +14,12 @@ echo Sending output to $output_file
 # Write headers to output file, if it does not yet exist
 if [[ ! -f $output_file ]] ; then
     echo Creating $output_file
-    $script_dir/speedtest-cli-extras/bin/speedtest-csv --header > $output_file
+    (echo -ne "pi_revision\\t"; $script_dir/speedtest-cli-extras/bin/speedtest-csv --header ) > $output_file
 fi
 
 # TODO Read https://www.raspberrypi.org/learning/networking-lessons/lessons/
 # TODO Include a column for hostname - so I can use hostname to indicate location
 # TODO Include a column for connection type - WiFi or Ethernet
-# TODO Include a column for Raspberry Pi type
 #  For WiFi: iwgetid --raw      ... gives ESSID:
 #            iwconfig           ... gives more info
 #  For Ethernet: ifconfig eth0
@@ -29,5 +28,7 @@ fi
 #       route(8), netstat(8), arp(8), rarp(8), iptables(8), ifup(8), interfaces(5).
 #       http://physics.nist.gov/cuu/Units/binary.html - Prefixes for binary multiples
 
-$script_dir/speedtest-cli-extras/bin/speedtest-csv --no-share >> $output_file
+# For meaning of pi_revision values, see https://elinux.org/RPi_HardwareHistory
+pi_revision=`cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'`
+(echo -ne "$pi_revision\\t"; $script_dir/speedtest-cli-extras/bin/speedtest-csv --no-share ) >> $output_file
 
