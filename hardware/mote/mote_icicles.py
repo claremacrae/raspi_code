2fe8a01ec34596612190baf59de054c5612e406e
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-12-23 23:17:01 +0000

    Attempt to allow icicles to drip in parallel. This basically works, but sometimes they are refreshed in the middle of updating other icicles, so some lights occasionally appear the wrong code, i.e. not white.

--- hardware/mote/mote_icicles.py
+++ hardware/mote/mote_icicles.py
@@ -1,4 +1,5 @@
 import time
+import threading
 from random import random, randint
 from mote import Mote
 
@@ -12,21 +13,50 @@ mote.configure_channel(4, 16, False)
 max_brightness = 40
 
 class Icicle:
-    def __init__(self, channnel):
+    def __init__(self, channel):
         self.channel = channel
     def run(self):
-        for pixel in range(16):
-            brightness = max_brightness -(2*pixel)
-            mote.set_pixel(channel, pixel, brightness, brightness, brightness)
-            mote.show()
-            time.sleep(0.05)
-            mote.set_pixel(channel, pixel, 0, 0, 0)
+        while True:
+            for pixel in range(16):
+                brightness = max_brightness -(2*pixel)
+                mote.set_pixel(self.channel, pixel, brightness, brightness, brightness)
+                mote.show()
+                time.sleep(0.05)
+                mote.set_pixel(self.channel, pixel, 0, 0, 0)
+            time.sleep(random() * 3.0)
 
-mote.clear()
-
-while True:
-    channel = randint(1, 4)
-    icicle = Icicle(channel)
+def thread_icicle1():
+    icicle = Icicle(1)
     icicle.run()
-    mote.show()
-    time.sleep(random() * 3.0)
+
+def thread_icicle2():
+    icicle = Icicle(2)
+    icicle.run()
+
+def thread_icicle3():
+    icicle = Icicle(3)
+    icicle.run()
+
+def thread_icicle4():
+    icicle = Icicle(4)
+    icicle.run()
+
+if __name__ == "__main__":
+    mote.clear()
+
+    t1 = threading.Thread(name="Hello1", target=thread_icicle1)
+    t1.start()
+
+    t2 = threading.Thread(name="Hello2", target=thread_icicle2)
+    t2.start()
+
+    t3 = threading.Thread(name="Hello3", target=thread_icicle3)
+    t3.start()
+
+    t4 = threading.Thread(name="Hello4", target=thread_icicle4)
+    t4.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
