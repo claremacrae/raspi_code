4b8f3e426fa7ced9900b5626964ea06f0913f746
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2018-12-23 22:25:21 +0000

    First stab at showing icicles on Pimoroni Mote

---
+++ hardware/mote/mote_icicles.py
@@ -0,0 +1,30 @@
+import time
+from mote import Mote
+
+mote = Mote()
+
+mote.configure_channel(1, 16, False)
+mote.configure_channel(2, 16, False)
+mote.configure_channel(3, 16, False)
+mote.configure_channel(4, 16, False)
+
+max_brightness = 40
+
+class Icicle:
+    def __init__(self, channnel):
+        self.channel = channel
+    def run(self):
+        for pixel in range(16):
+            mote.clear()
+            brightness = max_brightness -(2*pixel)
+            mote.set_pixel(channel, pixel, brightness, brightness, brightness)
+            mote.show()
+            time.sleep(0.05)
+            mote.set_pixel(channel, pixel, 0, 0, 0)
+
+mote.clear()
+
+while True:
+    for channel in range(1, 5):
+        icicle = Icicle(channel)
+        icicle.run()
