ace9841a7c699dd80915af05c0589b236e45b521
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2021-06-10 14:10:23 +0100

    Demo handling middle button push on Sense HAT

--- hardware/sense_hat/demo_buttons.py
+++ hardware/sense_hat/demo_buttons.py
@@ -9,6 +9,10 @@ x = 3
 y = 3
 sense = SenseHat()
 
+color1 = [255, 255, 255]
+color2 = [255, 0, 0]
+current_color = color1
+
 def clamp(value, min_value=0, max_value=7):
     return min(max_value, max(min_value, value))
 
@@ -32,14 +36,23 @@ def pushed_right(event):
     if event.action != ACTION_RELEASED:
         x = clamp(x + 1)
 
+def pushed_middle(event):
+    global current_color
+    if event.action != ACTION_RELEASED:
+        if current_color == color1:
+            current_color = color2
+        else:
+            current_color = color1
+
 def refresh():
     sense.clear()
-    sense.set_pixel(x, y, 255, 255, 255)
+    sense.set_pixel(x, y, current_color[0], current_color[1], current_color[2])
 
 sense.stick.direction_up = pushed_up
 sense.stick.direction_down = pushed_down
 sense.stick.direction_left = pushed_left
 sense.stick.direction_right = pushed_right
+sense.stick.direction_middle = pushed_middle
 sense.stick.direction_any = refresh
 refresh()
 pause()
